23be7cb3d8f2482f110fd112711165f7d7b401db
arjun malgwa <123358783+Arjun-malgwa@users.noreply.github.com>
2025-10-31 19:27:50 -0400

    Add Vite configuration for React project

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  // For a user site repo (arjun-malgwa.github.io), base can stay default "/"
+})
 
